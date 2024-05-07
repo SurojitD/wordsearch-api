f55f41c5077f6193c3d0afd7a316dd50bd9fa5d9
Surojit <surojit.datta2012@yahoo.in>
2024-05-07 10:30:58 -0700

    Wordsearch API implementation

---
+++ src/test/java/datta/surojit/wordsearchapi/WordsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package datta.surojit.wordsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
